5f31141a71c0309af4f1ba78fd4f1ede16e966a8
kieran coito <kierancotio@gmail.com>
2020-02-06 16:50:08 -0800

    memedro

--- initAntGrid.py
+++ initAntGrid.py
@@ -1,4 +1,20 @@
-import numpy
+import numpy as np
+import variables
+
+antProbability = 0.6
 
 def initAntGrid(n):
-    return numpy.zeros((n + 2, n + 2))
+    grid = np.array((n + 2, n + 2))
+    grid[0:] = variables.BORDER
+    grid[-1:] = variables.BORDER
+    grid[:0] = variables.BORDER
+    grid[:-1] = variables.BORDER
+
+    for i in range(1, n-1):
+        for j in range(1, n-1):
+            if np.random.random() < antProbability:
+                randomDirection = np.random.randint(0,4)
+
+                grid[i][j] =
+
+    return
